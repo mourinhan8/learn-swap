72c36b93784702102bcf793b97b65b9e186dd2f5
ngocnhan <sbtc.walkingdead@gmail.com>
2023-12-09 16:52:40 +0700

    update script deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,21 +1,24 @@
-import { ethers } from "ethers";
+import { ethers, hardhatArguments } from 'hardhat';
+import * as Config from './config';
 
 async function main() {
+    await Config.initConfig();
+    const network = hardhatArguments.network ? hardhatArguments.network : 'dev';
     const [deployer] = await ethers.getSigners();
-  
-    console.log("Deploying contracts with the account:", deployer.address);
-  
-    console.log("Account balance:", (await deployer.getBalance()).toString());
-  
-    const Token = await ethers.getContractFactory("Floppy");
-    const token = await Token.deploy();
-  
-    console.log("Token address:", token.address);
-  }
-  
-  main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-      console.error(error);
-      process.exit(1);
-    });
+    console.log('deploy from address: ', deployer.address);
+    
+    const Auction = await ethers.getContractFactory("Auction");
+    const auction = await Auction.deploy("0xd54D6d5BD983a6cA18F8820f80E0A970FE4A9a8c", "0x65f00a282A58B30f8376D41832d76CeCB7b6186C");
+    console.log('Market deployed at: ', auction.address);
+    
+    Config.setConfig(network + '.Auction', auction.address);
+
+    await Config.updateConfig();
+    
+}
+
+main().then(() => process.exit(0))
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+});
